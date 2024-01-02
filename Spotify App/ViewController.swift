9dd824192e1de421b70ba9f0263ef4de8229c284
zhshakuali <zhansayashakuali200309@gmail.com>
2024-01-02 17:44:27 +0600

    Initial Commit

---
+++ Spotify App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify App
+//
+//  Created by Жансая Шакуали on 02.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
